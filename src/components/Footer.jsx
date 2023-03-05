7ab55a4ff5f660608dcf9ff881df087cb9fca45f
Agustin Rojas <agus.rojasmolina@gmail.com>
2023-03-05 14:48:13 -0300

    footer fix

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -18,14 +18,19 @@ function Footer() {
 
 
     return (
-        <div className='ml-[2%] h-[4rem] tablet:h-[4.2rem] absolute bottom-0 w-[85%]'>
-            <footer className='flex h-[4rem] justify-between' >
-                <div>
+        <div className='ml-[2%] h-[4rem] tablet:h-[4rem] absolute bottom-0 w-[95%] tablet:w-[85%]'>
+            <footer className='flex h-[4rem] justify-end tablet:justify-between' >
+                <div className='hidden tablet:block'>
                     <p></p>
                 </div>
-                <div className='flex flex-row h-[2rem] items-center'>
+                <div className='flex flex-col tablet:flex-row h-[2rem] items-end tablet:items-center mr-4 justify-end' >
                     <p className='flex gap-2'>2022 ® Caracolaracol <img src={logo2} className='w-6 h-6' /></p>
-                    <p className='hover:cursor-pointer hover:text-naranjahover ml-8 px-1 font-tommyregular rounded-sm active:bg-purpuraclaro active:bg-opacity-70' onClick={handlerSpanish}>Español</p><em className='text-naranjahover px-2 font-tommyregular'> // </em><p className='hover:cursor-pointer hover:text-naranjahover px-1 font-tommyregular rounded-sm active:bg-purpuraclaro active:bg-opacity-70' onClick={handlerEnglish}>English</p>
+                    <div className='flex items-center'>
+                        <p className='text-sm tablet:text-whitem text-naranja hover:cursor-pointer hover:text-naranjahover tablet:ml-8 px-1 font-tommyregular rounded-sm active:bg-purpuraclaro active:bg-opacity-70' onClick={handlerSpanish}>Español</p>
+                        <em className='text-naranjahover px-2 font-tommyregular'> // </em>
+                        <p className='text-sm tablet:text-whitem text-naranja hover:cursor-pointer hover:text-naranjahover tablet:px-1 font-tommyregular rounded-sm active:bg-purpuraclaro active:bg-opacity-70' onClick={handlerEnglish}>English</p>
+                    </div>
+                    
                 </div>
             </footer>
         </div>
